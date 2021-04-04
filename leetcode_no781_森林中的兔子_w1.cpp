caf5e897b31f86f6fd5c31bee756a0b7f3129521
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-04-04 18:14:17 +0800

    Add files via upload

---
+++ leetcode_no781_森林中的兔子_w1.cpp
@@ -0,0 +1,100 @@
+﻿/* ------------------------------------------------------------------|
+森林中，每个兔子都有颜色。其中一些兔子（可能是全部）告诉你还有多少其他的兔子和自己有相同的颜色。我们将这些回答放在 answers 数组里。
+
+返回森林中兔子的最少数量。
+
+示例:
+输入: answers = [1, 1, 2]
+输出: 5
+解释:
+两只回答了 "1" 的兔子可能有相同的颜色，设为红色。
+之后回答了 "2" 的兔子不会是红色，否则他们的回答会相互矛盾。
+设回答了 "2" 的兔子为蓝色。
+此外，森林中还应有另外 2 只蓝色兔子的回答没有包含在数组中。
+因此森林中兔子的最少数量是 5: 3 只回答的和 2 只没有回答的。
+
+输入: answers = [10, 10, 10]
+输出: 11
+
+输入: answers = []
+输出: 0
+说明:
+
+answers 的长度最大为1000。
+answers[i] 是在 [0, 999] 范围内的整数。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/rabbits-in-forest
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	贪心法
+*
+*	执行用时：4 ms, 在所有 C++ 提交中击败了88.05%的用户
+*	内存消耗：8.3 MB, 在所有 C++ 提交中击败了62.15%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+int numRabbits(vector<int>& answers) {
+    unordered_map<int, int> map;
+    int res = 0;
+    for (int& i : answers) {
+        //如果不存在这个数，则至少还有i+1个没计到
+        //增加计数，插入索引
+        if (map.find(i) == map.end()) {
+            res += i + 1;
+            map[i] = i;
+        }
+        else {
+            //如果还有库存，就-1
+            if (map[i]) {
+                map[i]--;
+            }
+            //否则说明当前颜色填完了，要开新的颜色
+            else {
+                res += i + 1;
+                map[i] = i;
+            }
+        }
+    }
+    return res;
+}
